10d5091c018b1b250da706af9e990b5e9cea7d8f
Robin Freyler <robin.freyler@gmail.com>
2022-01-05 22:21:34 +0100

    clean up tests

--- src/lib.rs
+++ src/lib.rs
@@ -15,6 +15,12 @@ use self::context::{Engine, ExecutionContext, Func, Function, Instruction, Value
 use self::error::Trap;
 use self::register::Register;
 
+/// Creates a function that takes a parameter `n` and
+/// counts from `n` down to 0 before it returns 0.
+///
+/// # Note
+///
+/// This is a function intended for benchmarking purposes.
 fn counter_loop() -> Function {
     Function::build(1, 0, 0)
         .push_inst(Instruction::dup()) // 2
@@ -28,27 +34,41 @@ fn counter_loop() -> Function {
         .finish()
 }
 
+/// Creates a function that takes 2 parameters `a` and `b`.
+///
+/// # Note
+///
+/// - Performs the following calculation: (a+b)*(a+b)
+/// - Returns the result of the calculation.
+fn add_mul() -> Function {
+    Function::build(2, 1, 0)
+        .push_inst(Instruction::i32_add())
+        .push_inst(Instruction::dup())
+        .push_inst(Instruction::i32_mul())
+        .push_inst(Instruction::ret())
+        .finish()
+}
+
 #[test]
-fn it_works() {
+fn add_mul_works() {
     let mut engine = Engine::default();
-    // let function = Function::build(3, 1, 0)
-    //     .push_inst(Instruction::i32_add())
-    //     .push_inst(Instruction::i32_mul())
-    //     .push_inst(Instruction::i32_const(42))
-    //     .push_inst(Instruction::drop())
-    //     .push_inst(Instruction::ret())
-    //     .finish();
-    // let func = engine.push_function(function);
-    // let mut results = [Register::default(); 1];
-    // let result = engine.execute(
-    //     func,
-    //     &[Register::from(1), Register::from(2), Register::from(3)],
-    //     &mut results,
-    // );
-    // println!("result = {:?}", result);
+    let add_mul = engine.push_function(add_mul());
+    let mut results = [Register::default(); 1];
+    engine
+        .execute(
+            add_mul,
+            &[Register::from(2), Register::from(3)],
+            &mut results,
+        )
+        .unwrap();
+    assert_eq!(results[0], Register::from((2 + 3) * (2 + 3)));
+}
+
+#[test]
+fn counter_loop_works() {
+    let mut engine = Engine::default();
     let cl = engine.push_function(counter_loop());
-    let result = engine.execute(cl, &[Register::from(10_000)], &mut []);
-    // println!("result = {:?}", result);
+    engine.execute(cl, &[Register::from(10_000)], &mut []).unwrap();
 }
 
 #[bench]
@@ -56,6 +76,8 @@ fn bench_counter_loop(b: &mut Bencher) {
     let mut engine = Engine::default();
     let cl = engine.push_function(counter_loop());
     b.iter(|| {
-        engine.execute(cl, &[Register::from(100_000)], &mut []).unwrap()
+        engine
+            .execute(cl, &[Register::from(100_000)], &mut [])
+            .unwrap()
     })
 }
